3876503d57007cd7e857dfbf5b65293150b9dc87
Joel <joelmounts@gmail.com>
2023-06-20 21:07:58 -0400

    take 78

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({'strict':false})
+		adapter: adapter()
 	}
 };
 
