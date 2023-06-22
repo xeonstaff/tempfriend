e459a398f44d0565094b93d0c7de8cbadbb34bbf
Joel <joelmounts@gmail.com>
2023-06-22 16:23:13 -0400

    reason 15

--- src/routes/api/chatBot.js
+++ src/routes/api/chatBot.js
@@ -22,7 +22,7 @@ export async function chatBot() {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
-			Authorization: `Bearer ${process.env.OPENAI_API_KEY}`
+			Authorization: `Bearer ${VITE_OPENAI_API_KEY}`
 		},
 		body: JSON.stringify(data)
 	})
