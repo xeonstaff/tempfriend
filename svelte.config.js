e175f54c56860c66c59240096f0d8910e786e06d
Joel <joelmounts@gmail.com>
2023-06-22 15:32:32 -0400

    adding rollup to fix production api key

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,19 @@
 import adapter from '@sveltejs/adapter-vercel';
+import replace from '@rollup/plugin-replace';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter()
-	}
+	},
+	rollup: {
+		plugins: [
+			replace({
+				// Replace process.env values with their actual values in production
+				'process.env.OPENAI_API_KEY': JSON.stringify(process.env.OPENAI_API_KEY),
+			}),
+		],
+	},
 };
 
 export default config;
