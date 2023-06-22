5bf72556c30374ce51dbb544cd72eeffb171cca4
Joel <joelmounts@gmail.com>
2023-06-22 16:08:57 -0400

    revert

--- svelte.config.js
+++ svelte.config.js
@@ -5,15 +5,7 @@ import replace from '@rollup/plugin-replace';
 const config = {
 	kit: {
 		adapter: adapter()
-	},
-	rollup: {
-		plugins: [
-			replace({
-				// Replace process.env values with their actual values in production
-				'process.env.OPENAI_API_KEY': JSON.stringify(process.env.OPENAI_API_KEY),
-			}),
-		],
-	},
+	}
 };
 
 export default config;
