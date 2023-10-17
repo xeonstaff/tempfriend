87aaae82f1ba83fe85a1aed49369bf4f29021099
Joel Mounts <joel@JM.lan>
2023-10-16 22:06:16 -0400

    delete build

--- .vercel/output/functions/render.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,7 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.5dc1da13.js","_app/immutable/chunks/index.759e29a4.js","_app/immutable/chunks/singletons.0efed360.js","_app/immutable/chunks/index.99454fbc.js"];
-export const stylesheets = [];
-export const fonts = [];
