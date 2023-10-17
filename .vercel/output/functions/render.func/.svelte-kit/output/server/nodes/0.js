7d0df9ca85749edfc73484e21c4da991d38a957f
Joel Mounts <joel@JM.lan>
2023-10-16 22:19:13 -0400

    unbuild?

--- .vercel/output/functions/render.func/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,7 +0,0 @@
-
-
-export const index = 0;
-export const component = async () => (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.da6dc07e.js","_app/immutable/chunks/index.759e29a4.js","_app/immutable/chunks/fetch-npm-browserify.a89ed368.js","_app/immutable/chunks/index.99454fbc.js"];
-export const stylesheets = ["_app/immutable/assets/0.02237f52.css"];
-export const fonts = [];
--- .vercel/output/functions/render.func/.svelte-kit/output/server/nodes/2.js
+++
@@ -1,7 +0,0 @@
-
-
-export const index = 2;
-export const component = async () => (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.4e3d80fc.js","_app/immutable/chunks/index.759e29a4.js","_app/immutable/chunks/fetch-npm-browserify.a89ed368.js","_app/immutable/chunks/index.99454fbc.js"];
-export const stylesheets = ["_app/immutable/assets/2.ea76fe16.css"];
-export const fonts = [];
